0925fd2556ea8f357a0872cc32dfe08323db3ae5
Alice Li <nameal1234@gmail.com>
2024-07-13 12:04:36 -0700

    add transiitions between pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import Connect from "../src/pages/Connect.jsx";
 import About from "../src/pages/About.jsx";
 import Points from "../src/pages/Points.jsx";
@@ -8,18 +13,32 @@ import Home from "../src/pages/Home.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import Footer from "./components/Footer.js";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.key} timeout={500} classNames="fade">
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/connect" element={<Connect />} />
+          <Route path="/points" element={<Points />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+}
+
 function App() {
   return (
     <div>
       <Router>
         <div>
           <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/connect" element={<Connect />} />
-            <Route path="/points" element={<Points />} />
-          </Routes>
+          <AnimatedRoutes />
           <Footer />
         </div>
       </Router>
